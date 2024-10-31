d8a523ce0a219cd3489c5193458590e874e7ee30
uahmad417 <18u.ahmad@gmail.com>
2024-10-31 17:14:29 +0200

    US-3 implemented test case 3 - word end with consonant. also added docstrings to tests

--- piglatin.py
+++ piglatin.py
@@ -10,8 +10,10 @@ class PigLatin:
     def translate(self) -> str:
         if self.phrase == "":
             return "nil"
-        if self.phrase.endswith("y"):
-            return self.phrase + "nay"
-        if self.phrase[-1] in "aeiou":
+        if self.phrase[-1] in "aeiouAEIOU":
             return self.phrase + "yay"
+        elif self.phrase[-1] == "y":
+            return self.phrase + "nay"
+        else:
+            return self.phrase + "ay"
 
--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -8,17 +8,31 @@ from error import PigLatinError
 class TestPigLatin(unittest.TestCase):
 
     def test_get_phrase(self):
+        """to test if the phrase given as input is returned as is"""
+
         translator = PigLatin("hello world")
         self.assertEqual("hello world", translator.get_phrase())
 
     def test_translate_empty_string(self):
+        """test if an empty word returns `nil` as string """
+
         translator = PigLatin("")
         self.assertEqual("nil", translator.translate())
 
     def test_translate_word_ends_with_y(self):
+        """test if a word ending with `y` is appended with `nay` when translated"""
+
         translator = PigLatin("any")
         self.assertEqual("anynay", translator.translate())
 
     def test_translate_word_ends_with_vowel(self):
+        """test if a word ending with a vowel is appended with `yay` when translated"""
+
         translator = PigLatin("apple")
         self.assertEqual("appleyay", translator.translate())
+
+    def test_translate_word_ends_with_consonant(self):
+        """test if a word ending with a consonant is appended with `ay` when translated"""
+
+        translator = PigLatin("ask")
+        self.assertEqual("askay", translator.translate())
